24a31c1d9edc9c3dcf30f43d88eedb624c916df7
toqaewa <kath.tokaeva@gmail.com>
2025-06-12 15:22:36 +0300

    types export

--- types/ReservationTypes.ts
+++ types/ReservationTypes.ts
@@ -1,6 +1,6 @@
-type Status = 'notConfirmed' | 'confirmed' | 'started';
+export type Status = 'notConfirmed' | 'confirmed' | 'started';
 
-interface Reservation {
+export interface Reservation {
   id: string;
   guest: string;
   table: string;
@@ -12,7 +12,7 @@ interface Reservation {
   date: string; // YYYY-MM-DD
 }
 
-const STATUS_CONFIG = {
+export const STATUS_CONFIG = {
   notConfirmed: { label: 'Not Confirmed', color: '#FFA726' },
   confirmed: { label: 'Confirmed', color: '#29B6F6' },
   started: { label: 'Started', color: '#66BB6A' },
